3535f1a42953078d82e4afe9330b2f603e6010c4
Avishek Verma <avishekverma298@gmail.com>
2024-02-24 13:29:13 +0530

    Basic Auth implemented

---
+++ src/main/java/com/blog/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.blog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blog.entities.User;
+import com.blog.excep.UserNotFoundException;
+import com.blog.repo.UserRepo;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		// loading user from database by username
+		User user = userRepo.findByEmail(username).orElseThrow(()->new UserNotFoundException("User","email", username));
+		
+		return user;
+	}
+
+	
+}
