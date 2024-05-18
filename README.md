7977951bb24c3109a8f5499d1402d458fea652f4
Avishek Verma <avishekverma298@gmail.com>
2024-05-18 09:19:22 +0530

    Updated readme file

--- README.md
+++ README.md
@@ -1,39 +1,40 @@
+# Blogging App APIs
+This repo is backend for **Blogging App**. It contains all the backend REST APIs from which frontend can interact and get the request and response from backend.
+### Overview
+As part of _Blogging App_ user or client have a place where they can write and post their blogs and articles.
 
+Reader can read the published article and also able to comment on the articles.
 
-Normal Text
+### Features
+* User can __Create__, **Update**, **Delete** and __Pulish__ there article.
+* User or reader can __Add__, __Update__, __Delete__ comments on articles.
+* User can __Categories__ the articles according to the categories.
+* Existing user get __authenticated__ while loging to our application.
+* __New user__ can able to __Register__ on our application.
+* Every article should have at least one picture.
+* Have pagination and sorting if no. of articles on a page is more than 10.
 
-# Heading 1
-## Heading 2
-### Heading 3
+### Technical features
+* Login and Register API with proper authentication.
+* Validation for strong password while registering to the app.
+* User input validation handling.
+* Proper REST APIs end point.
+* Exception handling.
+* Swagger integration for proper API documentation.
+* Role based authentication and access of APIs.
+* JWT based authentication.
+* Deployment of application of AWS ec2 instance.
 
-_Italics_
-**Strong /Bold**
-~~1000~~
-[Visit my Repo](https://github.com/AvishekVerma/blogApp-apis "Blog Api project")
-
-![My image](https://www.peakpx.com/en/hd-wallpaper-desktop-gcdim)
-
-Use `for` loop
-
-```java
-public static String name = "Avishek";
-System.out.println("My name is "+ name);
-```
-
-Table
-
-|Table | Goes | Here
-|---|---|---|
-|One|Two|Three
-
->Keep smiling
-
----
-1. List one
-2. List two
-   3. List three
-      List four
-
-***
-
+## Tech Stack 
+* Java 8
+* Spring Core | Spring MVC | Spring Data JPA | Spring Security
+* MySQL DataBase
+* Postman Rest Client
+* Swagger
+* Maven
+* Apache Tomcat server
+* AWS-EC2 instance
+* Eclipse
+      
+      
 
--- src/main/java/com/blog/config/SwaggerConfig.java
+++ src/main/java/com/blog/config/SwaggerConfig.java
@@ -1,26 +1,57 @@
 package com.blog.config;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
+	
+	public static final String AUTHORIZATION_HEADER ="Authorization";
 
+	private ApiKey apiKeys() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+	
+	private List<SecurityContext> securityContext(){
+		return Arrays.asList(SecurityContext.builder().securityReferences(sf()).build());
+	}
+	
+	private List<SecurityReference> sf(){
+		
+		AuthorizationScope scope = new AuthorizationScope("global", "accessEverything");
+		
+		return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[] {scope}));
+	}
+	
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(getInfo()).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		System.out.println("inside swaggerConfig class");
+		
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(getInfo())
+				.securityContexts(securityContext())
+				.securitySchemes(Arrays.asList(apiKeys()))
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 	private ApiInfo getInfo() {
